49edeabdbd178268b1fd04dda0fe2436731ada48
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-09-22 23:02:03 -0700

    更多单行重命名测试, 包含//注释

--- src/com/codeinchinese/源码/翻译/功用.java
+++ src/com/codeinchinese/源码/翻译/功用.java
@@ -3,7 +3,14 @@ package com.codeinchinese.源码.翻译;
 public class 功用 {
 
   public static String 重命名(String 代码行, String 英文, String 中文) {
-    return 代码行.replace(英文, 中文);
+    String 注释 = "";
+    String 清理后 = 代码行;
+    if (代码行.contains("//")) {
+      int 注释位置 = 代码行.indexOf("//");
+      注释 = 代码行.substring(注释位置, 代码行.length());
+      清理后 = 代码行.substring(0, 注释位置);
+    }
+    return 清理后.replace(英文, 中文) + 注释;
   }
 
 }
--- test/ark_compiler_source_translation/标识符重命名Test.java
+++ test/ark_compiler_source_translation/标识符重命名Test.java
@@ -9,8 +9,30 @@ import com.codeinchinese.源码.翻译.功用;
 class 标识符重命名Test {
 
   @Test
-  void 重命名() {
-    assertEquals("class 编译器类 {", 功用.重命名("class Compiler {", "Compiler", "编译器类"));
+  void 行内重命名() {
+    // 单个标识符
+    assertEquals("class 编译器类 {",
+        功用.重命名("class Compiler {", "Compiler", "编译器类"));
+
+    // 多个同名标识符
+    assertEquals("  explicit 编译器类(const std::string &名称) : 名称(名称) {}",
+        功用.重命名("  explicit 编译器类(const std::string &name) : name(name) {}",
+            "name", "名称"));
+
+    // 大小写敏感
+    assertEquals("  const int 运行(const 选项类 &MplOptions, const std::string &普通选项) const;",
+        功用.重命名("  const int 运行(const 选项类 &MplOptions, const std::string &options) const;",
+            "options", "普通选项"));
+
+    // 无视//打头的行
+    assertEquals("// options",
+        功用.重命名("// options",
+            "options", "普通选项"));
+
+    // 无视//之后部分
+    assertEquals("普通选项// options",
+        功用.重命名("options// options",
+            "options", "普通选项"));
   }
 
 }
